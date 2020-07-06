13694926703a2fb67e41f812ddc225045a69badf
Daman56100 <samcraft792@gmail.com>
2020-07-06 21:17:54 +0100

    Forgot to update to dfplusplus in the build script

--- generateRankedJars.sh
+++ generateRankedJars.sh
@@ -3,12 +3,12 @@ echo Building the other jars from $1
 # removes previous build files, files in the top directory NOT matching the name in $1
 find build/libs/*.* ! -name $1.jar -exec rm -f {} \;
 
-# ensure the dfadmintools folder exists
-mkdir -p build/libs/assets/dfadmintools
+# ensure the dfplusplus folder exists
+mkdir -p build/libs/assets/dfplusplus
 
 # copies permission files in
 # shellcheck disable=SC2225
-cp -a permissions/. build/libs/assets/dfadmintools
+cp -a permissions/. build/libs/assets/dfplusplus
 
 # cds to make the next bit more readable
 # shellcheck disable=SC2164
@@ -16,11 +16,11 @@ cd build/libs
 
 # creates new jar then copies the file in
 cp $1.jar $1-admin.jar
-jar uf $1-admin.jar assets/dfadmintools/admin_permissions
+jar uf $1-admin.jar assets/dfplusplus/admin_permissions
 
 cp $1.jar $1-mod.jar
-jar uf $1-mod.jar assets/dfadmintools/mod_permissions
+jar uf $1-mod.jar assets/dfplusplus/mod_permissions
 
 cp $1.jar $1-support.jar
-jar uf $1-support.jar assets/dfadmintools/support_permissions
+jar uf $1-support.jar assets/dfplusplus/support_permissions
 
