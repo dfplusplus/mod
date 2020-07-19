e0dca79a1058a7f9001ae44aa880bf99d6bb6dc7
Daman56100 <samcraft792@gmail.com>
2020-07-19 10:34:47 +0100

    Added syncWithMinecraft, chatScale and chatWidth fields If syncWithMinecraft is true, the getter returns the default minecraft settings, else returns the custom ones.

--- src/main/java/com/github/dfplusplus/chat/screens/ChatSizingScreen.java
+++ src/main/java/com/github/dfplusplus/chat/screens/ChatSizingScreen.java
@@ -1,6 +1,7 @@
 package com.github.dfplusplus.chat.screens;
 
 import com.github.dfplusplus.chat.ChatGuiOverride;
+import com.github.dfplusplus.chat.ChatRule;
 import net.minecraft.client.GameSettings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.IGuiEventListener;
@@ -9,6 +10,7 @@ import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.OptionSlider;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.AbstractOption;
 import net.minecraft.client.settings.SliderPercentageOption;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -20,6 +22,13 @@ public class ChatSizingScreen extends Screen {
     private final Screen priorScreen;
     private static double chatOffsetXValue = 0;
     private static double chatOffsetYValue = 0;
+    private static boolean syncWithMinecraft = true;
+    private static double chatScale;
+    private static double chatWidth;
+
+    private Button toggleSyncButton;
+    private OptionSlider chatScaleSlider;
+    private OptionSlider chatWidthSlider;
 
     public static int getChatOffsetX() {
         return ((int) chatOffsetXValue);
@@ -27,6 +36,14 @@ public class ChatSizingScreen extends Screen {
 
     public static int getChatOffsetY() {
         return (int) chatOffsetYValue;
+    }
+
+    public static double getChatScale() {
+        return syncWithMinecraft ? Minecraft.getInstance().gameSettings.chatScale : chatScale;
+    }
+
+    public static double getChatWidth() {
+        return syncWithMinecraft ? Minecraft.getInstance().gameSettings.chatWidth : chatWidth;
     }
 
     public ChatSizingScreen(Screen priorScreen) {
@@ -37,28 +54,70 @@ public class ChatSizingScreen extends Screen {
     }
 
     @Override
-    protected void init() {
-        super.init();
-//        inputField.setEnabled(false);
+    public void init(Minecraft p_init_1_, int p_init_2_, int p_init_3_) {
+        super.init(p_init_1_, p_init_2_, p_init_3_);
+        // back button
         if (priorScreen != null) addButton(new Button(10,10,50,20,"Back",this::onBackButtonPress));
+
+        // chat offset x slider
         int windowWidth = minecraft.getMainWindow().getScaledWidth();
         this.addButton(new OptionSlider(
                 minecraft.gameSettings, (width / 2) - 100, 20, 200, 20,
                 new SliderPercentageOption("gui.dfplusplus.chatoffsetx", -windowWidth/2f, windowWidth/2f, 1f,
                         (gameSettings -> chatOffsetXValue),
-                        ((gameSettings, aDouble) -> chatOffsetXValue = aDouble), // %% is escaped to %
+                        ((gameSettings, aDouble) -> chatOffsetXValue = aDouble),
                         (gameSettings, sliderPercentageOption) -> String.format("Chat Offset X: %spx", ((int) chatOffsetXValue))
                 ))
         );
 
+        // chat offset y slider
         this.addButton(new OptionSlider(
                 minecraft.gameSettings, (width / 2) - 100, 45, 200, 20,
                 new SliderPercentageOption("gui.dfplusplus.chatoffsetx", 0f, minecraft.getMainWindow().getScaledHeight(), 1f,
                         (gameSettings -> chatOffsetYValue),
-                        ((gameSettings, aDouble) -> chatOffsetYValue = aDouble), // %% is escaped to %
+                        ((gameSettings, aDouble) -> chatOffsetYValue = aDouble),
                         (gameSettings, sliderPercentageOption) -> String.format("Chat Offset Y: %spx", ((int) chatOffsetYValue))
                 ))
         );
+
+        // sync toggle button
+        this.toggleSyncButton = this.addButton(new Button(
+                (width/2) - 100, 70, 200, 20, "", (button) -> {
+            syncWithMinecraft = !syncWithMinecraft;
+            this.init(minecraft,width,height);
+        }
+        ));
+        this.toggleSyncButton.setMessage(I18n.format(syncWithMinecraft ? "gui.dfplusplus.settings.sync.true" : "gui.dfplusplus.settings.sync.false"));
+
+        // chat scale slider
+        this.chatScaleSlider = this.addButton(new OptionSlider(
+                minecraft.gameSettings, (width / 2) - 100, 95, 200, 20,
+                new SliderPercentageOption("gui.dfplusplus.chatscale", 0, 1, 0,
+                        (gameSettings -> getActualChatScale()), // on get
+                        ((gameSettings, aDouble) -> {
+                            if (!syncWithMinecraft) chatScale = aDouble;
+                            minecraft.ingameGUI.persistantChatGUI.clearChatMessages(false);
+                        }), // on set, only works if syncing
+                        (gameSettings, sliderPercentageOption) -> String.format("Chat Scale: %s%%", (int)(getActualChatScale() * 100)) // on display
+                )
+        ));
+        this.chatScaleSlider.active = !syncWithMinecraft;
+
+        // chat width slider
+        this.chatWidthSlider = this.addButton(new OptionSlider(
+                minecraft.gameSettings, (width / 2) - 100, 120, 200, 20,
+                new SliderPercentageOption("gui.dfplusplus.chatwidth", 0, 1, 0,
+                        (gameSettings -> getActualChatWidth()), // on get
+                        ((gameSettings, aDouble) -> {
+                            if (!syncWithMinecraft) chatWidth = aDouble;
+                            minecraft.ingameGUI.persistantChatGUI.clearChatMessages(false);
+                        }), // on set, only works if syncing
+                        (gameSettings, sliderPercentageOption) -> String.format("Chat Width: %spx", NewChatGui.calculateChatboxWidth(getActualChatWidth())) // on display
+                )
+        ));
+
+        // dummy message button
+        addButton(new Button((width / 2) ,145,200,20,"Send Dummy Messages",this::onDummyMessagesButtonPress));
     }
 
     @Override
@@ -69,5 +128,19 @@ public class ChatSizingScreen extends Screen {
 
     private void onBackButtonPress(Button button) {
         minecraft.displayGuiScreen(priorScreen);
+    }
+
+    private void onDummyMessagesButtonPress(Button button) {
+        ChatGuiOverride chatGuiOverride = ((ChatGuiOverride) minecraft.ingameGUI.persistantChatGUI);
+        chatGuiOverride.addToChat(ChatRule.ChatSide.MAIN, "TEST");
+        chatGuiOverride.addToChat(ChatRule.ChatSide.SIDE, "TEST2");
+    }
+
+    private double getActualChatScale() {
+        return syncWithMinecraft ? minecraft.gameSettings.chatScale : chatScale;
+    }
+
+    private double getActualChatWidth() {
+        return syncWithMinecraft ? minecraft.gameSettings.chatWidth : chatWidth;
     }
 }
