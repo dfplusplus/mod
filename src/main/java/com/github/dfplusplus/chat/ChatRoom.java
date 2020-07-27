2fe858b2a0a02161e7df322c62338e31e02bbecc
Daman56100 <samcraft792@gmail.com>
2020-07-27 11:08:47 +0100

    Added a permission level check This means that ChatRoom#getCustomChatrooms only returns chat rooms you have permission for

--- src/main/java/com/github/dfplusplus/chat/ChatRoom.java
+++ src/main/java/com/github/dfplusplus/chat/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.github.dfplusplus.chat;
 
+import com.github.dfplusplus.PermissionLevel;
 import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
 
@@ -7,19 +8,26 @@ import java.awt.*;
 import java.util.List;
 
 public enum ChatRoom {
-    DEFAULT_CHAT(Minecraft.getInstance().gameSettings.getChatBackgroundColor(Integer.MIN_VALUE)),
-    SUPPORT_CHAT((new Color(0x55,0xff,0xff,100)).getRGB()),
-    MOD_CHAT((new Color(0x55,0xff,0x55,100).getRGB())),
-    ADMIN_CHAT((new Color(0xff,0x55,0x55,100).getRGB()));
+    DEFAULT_CHAT(Minecraft.getInstance().gameSettings.getChatBackgroundColor(Integer.MIN_VALUE), PermissionLevel.DEFAULT),
+    SUPPORT_CHAT((new Color(0x55,0xff,0xff,100)).getRGB(), PermissionLevel.SUPPORT),
+    MOD_CHAT((new Color(0x55,0xff,0x55,100).getRGB()), PermissionLevel.MOD),
+    ADMIN_CHAT((new Color(0xff,0x55,0x55,100).getRGB()), PermissionLevel.ADMIN);
 
     private final int color;
+
+    private final PermissionLevel permissionLevel;
 
     public int getColor() {
         return color;
     }
 
-    ChatRoom(int color) {
+    public PermissionLevel getPermissionLevel() {
+        return permissionLevel;
+    }
+
+    ChatRoom(int color, PermissionLevel permissionLevel) {
         this.color = color;
+        this.permissionLevel = permissionLevel;
     }
 
     /**
@@ -28,7 +36,8 @@ public enum ChatRoom {
      */
     public static List<ChatRoom> getCustomChatrooms() {
         List<ChatRoom> customChatrooms = Lists.newArrayList(ChatRoom.values());
-        customChatrooms.removeIf(chatRoom -> chatRoom == ChatRoom.DEFAULT_CHAT);
+        customChatrooms.removeIf(chatRoom -> chatRoom == ChatRoom.DEFAULT_CHAT); // remove default chat
+        customChatrooms.removeIf(chatRoom -> !PermissionLevel.hasPerms(chatRoom.getPermissionLevel())); // remove chats you dont have perms for
         return customChatrooms;
     }
 }
