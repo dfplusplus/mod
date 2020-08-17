9a3dd8d9f20a50c3b8c860900178d8ba76be9ac4
Daman56100 <samcraft792@gmail.com>
2020-08-17 18:41:50 +0100

    Added file copying. Also makes it readonly.

--- copy-common.sh
+++ copy-common.sh
@@ -1,1 +1,8 @@
-cp -r common/src/main/java/io forge/src/main/java/
+# removes old common files
+rm -rf forge/src/main/java/com/github/dfplusplus/common
+
+# copies the files
+cp -r common/src/main/java/com forge/src/main/java/
+
+# makes them readonly
+chmod -R -w forge/src/main/java/com/github/dfplusplus/common
