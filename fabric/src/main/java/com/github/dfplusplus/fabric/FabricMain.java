5d87eb8f4d31ee77e78cb920cf4212492d87940e
Daman56100 <samcraft792@gmail.com>
2020-08-20 17:46:53 +0100

    Update main file name.

--- fabric/src/main/java/com/github/dfplusplus/fabric/FabricMain.java
+++ fabric/src/main/java/com/github/dfplusplus/fabric/FabricMain.java
@@ -3,12 +3,11 @@ package com.github.dfplusplus.fabric;
 import com.github.dfplusplus.common.Test;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.KeyBinding;
 
-public class ExampleMod implements ModInitializer {
+public class FabricMain implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// This code runs as soon as MinecraftClient is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
