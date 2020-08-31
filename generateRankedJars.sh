296a6604448b7a46f7957bb6eae879f96ba7f01f
Daman56100 <samcraft792@gmail.com>
2020-08-31 15:29:05 +0100

    Modloader is added to the jar's name. So its now dfplusplus-ver-forge-admin.

--- generateRankedJars.sh
+++ generateRankedJars.sh
@@ -1,4 +1,4 @@
-echo Building the other jars from $1
+echo Building the other jars from $1 $2
 
 # removes previous build files, files in the top directory NOT matching the name in $1
 find build/libs/*.* ! -name $1.jar -exec rm -f {} \;
@@ -15,15 +15,15 @@ cp -a ../permissions/. build/libs/assets/dfplusplus
 cd build/libs
 
 # creates new jar then copies the file in
-cp $1.jar $1-admin.jar
-jar uf $1-admin.jar assets/dfplusplus/admin_permissions
+cp $1.jar $1-$2-admin.jar
+jar uf $1-$2-admin.jar assets/dfplusplus/admin_permissions
 
-cp $1.jar $1-mod.jar
-jar uf $1-mod.jar assets/dfplusplus/mod_permissions
+cp $1.jar $1-$2-mod.jar
+jar uf $1-$2-mod.jar assets/dfplusplus/mod_permissions
 
-cp $1.jar $1-expert.jar
-jar uf $1-expert.jar assets/dfplusplus/expert_permissions
+cp $1.jar $1-$2-expert.jar
+jar uf $1-$2-expert.jar assets/dfplusplus/expert_permissions
 
-cp $1.jar $1-support.jar
-jar uf $1-support.jar assets/dfplusplus/support_permissions
+cp $1.jar $1-$2-support.jar
+jar uf $1-$2-support.jar assets/dfplusplus/support_permissions
 
