1b19f82867444dae4c5eebd83424714a802f9362
DaMan56100 <samcraft792@gmail.com>
2020-12-30 15:46:53 +0000

    Added a logger to CommonMain.java. No more sout

--- common/src/main/java/com/github/dfplusplus/common/CommonMain.java
+++ common/src/main/java/com/github/dfplusplus/common/CommonMain.java
@@ -1,5 +1,10 @@
 package com.github.dfplusplus.common;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CommonMain {
     public static final String MOD_ID = "dfplusplus";
+
+    public static Logger LOGGER = LogManager.getLogger();
 }
